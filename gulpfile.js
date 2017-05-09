44e9c9e9e97176547fa6852c56a0d0552ebb8784
khawkins98 <khawkins98@gmail.com>
2017-05-09 16:44:24 +0100

    Update build for EBI infrastructure

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var gutil = require('gulp-util');
 var inlineImages = require('gulp-inline-images');
 var critical = require('critical').stream;
 var minifyInline = require('gulp-minify-inline');
+var through = require('through2');
 
 // Translate any src images to base64
 gulp.task('inline-images', function(cb){
-    return gulp.src(['src/*.html','src/*/*.html'])
+    return gulp.src(['src/*.html','src/*/*.html','src/*/*/*.html'])
       .pipe(inlineImages({/* options */}))
       .pipe(gulp.dest('dist/'));
 });
@@ -15,11 +16,11 @@ gulp.task('inline-images', function(cb){
 // https://github.com/addyosmani/critical
 // https://github.com/addyosmani/critical-path-css-demo
 gulp.task('critical', ['inline-images'], function (cb) {
-    return gulp.src(['dist/*.html','dist/*/*.html'])
-        // .pipe(critical({base: 'src/', inline: true, css: ['']}))
-        .pipe(critical({base: 'dist/', inline: true, width: 900, height: 2200, minify: true, ignore: [/icon-/,'@font-face']}))
-        .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['dist/*.html','dist/*/*.html','dist/*/*/*.html'])
+    // .pipe(critical({base: 'src/', inline: true, css: ['']}))
+    .pipe(critical({base: 'dist/', inline: true, width: 900, height: 2200, minify: true, ignore: [/icon-/,'@font-face']}))
+    .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
+    .pipe(gulp.dest('dist'));
 });
 
 // Minify any inline css/js
@@ -38,10 +39,35 @@ var optionsminify = {
 };
 
 gulp.task('minify-inline', ['critical'], function(cb) {
-  gulp.src(['dist/*.html','dist/*/*.html'])
+  gulp.src(['dist/*.html','dist/*/*.html','dist/*/*/*.html'])
     .pipe(minifyInline(optionsminify))
-    .pipe(gulp.dest('dist/'))
+    .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
+    .pipe(gulp.dest('dist/'));
+});
+
+// Write a partial apache config
+// https://github.com/ebiwd/EBI-Corporatesite/issues/1
+var pipeFunction = () => {
+  return through.obj((file, enc, cb) => {
+    console.log(file.path);
+    return cb(null, file);
+  });
+}
+gulp.task('apache-config', ['critical'], function(cb) {
+  var fileName = 'dist/.htaccess';
+  var endOfLine = '\r\n';
+  require('fs').writeFileSync(fileName, '# Static page mappings built with gulp');
+  gulp.src(['dist/*.html','dist/*/*.html','dist/*/*/*.html'])
+    // .pipe(pipeFunction());
+  .pipe(through.obj(function (file, enc, cb) {
+    var localFilePath = file.path.split('/dist/')[1];
+    console.log('Mapping: ',localFilePath);
+    require('fs').appendFile(fileName, endOfLine); // new line
+    require('fs').appendFile(fileName, 'RewriteRule ^/'+localFilePath.split('index.htm')[0]+'$ /staticpages/'+localFilePath+' [L]');
+    cb(null, file)
+  }));
+  // require('fs').appendFile(fileName, endOfLine); // new line
 });
 
 // Build it all
-gulp.task('default', ['inline-images','critical','minify-inline']);
+gulp.task('default', ['inline-images','critical','minify-inline','apache-config']);
