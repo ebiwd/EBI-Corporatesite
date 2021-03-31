e597063b188dd54eeb2c3b189b11103c4cd8ff02
Ken Hawkins <khawkins98@gmail.com>
2021-03-31 10:05:17 +0200

    Update path

--- eleventy.js
+++ eleventy.js
@@ -99,7 +99,7 @@ module.exports = function(config) {
     htmlTemplateEngine : ["njk", "md"],
     markdownTemplateEngine : "njk",
     passthroughFileCopy: true,
-    pathPrefix: "/ebi-eleventy-boilerplate/" // if your site is deployed to a sub-url, otherwise comment out
+    pathPrefix: "/" // if your site is deployed to a sub-url, otherwise comment out
   };
 
 };
--- src/site/_data/siteConfig.js
+++ src/site/_data/siteConfig.js
@@ -1,10 +1,10 @@
 // Some various reusable configuration
 module.exports = {
   siteInformation: {
-    title: "Eleventy for the VF 1.3 + 2.0",
+    title: "EMBL-EBI",
     short_description: 'This allows using the <a class="vf-link" href="https://www.11ty.io">11ty</a> static site generator with direct access to <a class="vf-link" href="https://github.com/visual-framework/vf-core">Visual Framework 2.0</a> and 1.3 code.',
-    url: "https://ebiwd.github.io/ebi-eleventy-boilerplate/",
-    author: "Visual Framework system",
+    url: "https://www.ebi.ac.uk/",
+    author: "EMBL-EBI Web Dev",
     email: "ken.hawkins@embl.de",
   }
 };
