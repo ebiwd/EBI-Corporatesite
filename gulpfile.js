2aa5a6698f8fb03517bb2496fbc1ebcedadd8e80
Ken Hawkins <khawkins98@gmail.com>
2021-04-01 10:41:55 +0200

    Feature: eleventy refactor (#27)

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,32 @@
+const path = require('path');
 const gulp = require('gulp');
 const gutil = require('gulp-util');
-const replace = require('gulp-replace');
-const inlineImages = require('gulp-inline-images');
-const minifyInline = require('gulp-minify-inline');
 const through = require('through2');
-const del = require('del');
-const browserSync = require('browser-sync').create();
 
-// Ensure dist folder is reset
-gulp.task('purge', function(cb){
-  return del([
-    'dist/*'
-  ]);
+// Pull in optional configuration from the package.json file, a la:
+const {componentPath, componentDirectories, buildDestionation} = require('@visual-framework/vf-config');
+
+// Tasks to build/run vf-core component system
+require('./node_modules/\@visual-framework/vf-core/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
+require('./node_modules/\@visual-framework/vf-extensions/gulp-tasks/_gulp_rollup.js')(gulp, path, componentPath, componentDirectories, buildDestionation);
+
+// Watch folders for changes
+gulp.task('watch', function() {
+  // left for convince for local watch additions
+  gulp.watch(['./build/css/styles.css'], gulp.series('eleventy:reload'));
 });
 
-// Translate any src images to base64
-gulp.task('inline-images', function(cb){
-  return gulp.src(['src/*.html','src/**/*.html'])
-    .pipe(inlineImages({/* options */}))
-    .pipe(gulp.dest('dist/'))
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red(err.message));
-      process.exit(1);
-    });
-});
-
-// Handle any jpg, svg, png assets
-gulp.task('file-assets', function(cb){
-  return gulp.src(['src/*.{png,gif,jpg,svg,pdf,mp4}','src/**/*.{png,gif,jpg,svg,pdf,mp4}'])
-    .pipe(gulp.dest('dist/'))
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red(err.message));
-      process.exit(1);
-    });
-});
-
-// Minify any inline css/js
-// https://www.npmjs.com/package/gulp-minify-inline
-const optionsminify = {
-  // js: {
-  //   output: {
-  //     comments: true
-  //   }
-  // },
-  jsSelector: 'script[type!="text/x-handlebars-template"]',
-  css: {
-    keepSpecialComments: 1
-  },
-  cssSelector: 'style[data-do-not-minify!="true"]'
-};
-
-gulp.task('minify-inline', function(cb) {
-  return gulp.src(['dist/*.html','dist/**/*.html'])
-    .pipe(replace(/('|")http(s)?\:\/\/www.ebi/g, '$1//www.ebi')) // make all http/s ebi urls //
-    .pipe(minifyInline(optionsminify))
-    .on('error', function(err) {
-      gutil.log(gutil.colors.red(err.message));
-      process.exit(1);
-    })
-    .pipe(gulp.dest('dist/'));
-});
-
-// Write a partial apache config
-// https://github.com/ebiwd/EBI-Corporatesite/issues/1
-const pipeFunction = () => {
-  return through.obj((file, enc, cb) => {
-    console.log(file.path);
-    return cb(null, file);
-  });
-};
 gulp.task('apache-config', function(cb) {
-  const fileName = 'dist/.htaccess';
+  const fileName = 'build/.htaccess';
   const endOfLine = '\r\n';
   require('fs').writeFileSync(fileName, '# Static page mappings built with gulp');
   require('fs').appendFileSync(fileName, endOfLine); // new line
   require('fs').appendFileSync(fileName, 'AddOutputFilterByType DEFLATE text/html');
   require('fs').appendFileSync(fileName, endOfLine); // new line
   require('fs').appendFileSync(fileName, 'RewriteCond %{QUERY_STRING} !(^|&)q=');
-  return gulp.src(['dist/*.{html,jpg,png,gif,pdf,mp4}','dist/**/*.{html,svg,jpg,png,gif,pdf,mp4}'])
+  return gulp.src(['build/*.{html,jpg,png,gif,pdf,mp4}','build/**/*.{html,svg,jpg,png,gif,pdf,mp4}'])
     .pipe(through.obj(function (file, enc, cb) {
-      const localFilePath = file.path.split('/dist/')[1];
+      const localFilePath = file.path.split('/build/')[1];
       gutil.log(gutil.colors.green('Mapping:',localFilePath));
       require('fs').appendFileSync(fileName, endOfLine); // new line
       require('fs').appendFileSync(fileName, 'RewriteRule ^/'+localFilePath.split('index.htm')[0]+'?$ /staticpages/'+localFilePath+' [L]');
@@ -95,29 +42,30 @@ gulp.task('apache-config', function(cb) {
     );
 });
 
-// Development server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./src"
-    }
-  });
-  gulp.watch('**/*.{png,gif,jpg,svg,html}').on('change', browserSync.reload);
-  // gulp.watch('**/*.{png,gif,jpg,svg}' , ['file-assets']);
-});
+// Let's build this sucker.
+gulp.task('build', gulp.series(
+  'vf-clean',
+  gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
+  'vf-css:production', //optimise, prefix css
+  'fractal:build',
+  'fractal',
+  'eleventy:init',
+  'eleventy:build',
+  'apache-config'
+));
 
+// Build and watch things during dev
+gulp.task('dev', gulp.series(
+  'vf-clean',
+  gulp.parallel('vf-css','vf-scripts','vf-component-assets'),
+  'fractal:development',
+  'fractal',
+  'eleventy:init',
+  'eleventy:develop',
+  gulp.parallel('watch','vf-watch')
+));
 
 // Build it all
 gulp.task('default', gulp.series(
-  'purge','file-assets','inline-images','minify-inline','apache-config'
-));
-
-// Alias for default
-gulp.task('build', gulp.series(
-  'default'
-));
-
-// Local development
-gulp.task('dev', gulp.series(
-  'browser-sync'
+  'build'
 ));
